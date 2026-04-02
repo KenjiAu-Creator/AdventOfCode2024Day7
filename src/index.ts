7e7f7085c451606475d5e9bb5339554f8ef3fcde
kenjiau-creator <kenji.au9@gmail.com>
2026-04-02 10:16:17 -0600

    Updated to just using brute force approach for both parts 1 and 2 to improve readability of code;

--- src/index.ts
+++ src/index.ts
@@ -9,70 +9,28 @@ import * as fs from 'node:fs';
  *
  * Since we want to explore ALL options this points to using a DFS or a BFS approach.
  *
- * @param filePath the file path to the data to be parsed to figure out the calibration data
+ * @param {string} filePath The file path to the data to be parsed to figure out the calibration data
+ * @param {boolean} concat  Flag to include concatenation operator
  *
  * @returns {number} the result of adding the first number in every row that meets the calibration equation requirement
 */
 function parseCalibration(filePath: string, concat: boolean = false): number {
     /**
-     * This function will use a DFS approach to recursively check every permutation of '+' or '*' between elements
-     * inside the equation. This function will return the target number if the equation is possible and will return 0 otherwise.
-     *
-     * @param {number} target               The first number in the equation that must be equal to the summation
-     * @param {string} nums                 The numbers in the array with the reference to the current plank
-     * @param {number} currentSum           The current sum value so far up to the calibrationIndex in nums array
-     * @param {number} index                The index indicating which plank is being calibrated. (Row in the txt file)
-     *
-     * @returns {number}                    Returns target value if the equation is true and 0 otherwise
-     */
-    function addOrMultiplyDfs(target: number, nums: string[], currentSum: number, index: number, plankIndex: number): number {
-        if (target === currentSum && !rowAdded.has(plankIndex)) {
-            rowAdded.add(plankIndex);
-            return target;
-        }
-        if (rowAdded.has(plankIndex)) {
-            // Skip this path if we already included the value
-            return 0;
-        }
-        if (index >= (nums.length)) {
-            // We know that we are at the last number in the array
-            return 0;
-        }
-        if (currentSum > target) {
-            // No longer go down this path if we exceed the target value
-            return 0;
-        }
-
-        // if we are below the target then we should either add or multiply
-        const nextString: string | undefined = nums[index];
-
-        if (nextString) {
-            let nValue = Number.parseInt(nextString);
-
-            // + path
-            let newSum = currentSum + nValue;
-            const path1 = addOrMultiplyDfs(target, nums, newSum, index + 1, plankIndex, concat);
-            if (path1) return path1;
-
-            // * path
-            let newMult = currentSum * nValue;
-            const path2 = addOrMultiplyDfs(target, nums, newMult, index + 1, plankIndex, concat);
-            if (path2) return path2;
-        }
-
-        return 0;
-    }
-
-    /**
      * This function will take in an array of numbers and return 2D array of expressions where each permutation of
-     * '+', '*', or '|' is in place between each number.
+     * '+', '*', or '||' is in place between each number.
      *
      * @param {string[]} nums   Array of numbers the expression will contain
+     * @param {boolean} concat    Flag to include concatenation operator
      *
      * @returns {string[][]}    Returns a 2D array of all permutations of possible expressions
      */
-    function generatePermutations(nums: string[]): string[][] {
-        const ops = ["+", "*", "||"];
+    function generatePermutations(nums: string[], concat: boolean): string[][] {
+        const ops = ["+", "*"];
+
+        if (concat) {
+            ops.push("||")
+        }
+
         let resultSet: string[][] = [];
 
         function dfs(index: number, set: string[]) {
@@ -108,8 +66,8 @@ function parseCalibration(filePath: string, concat: boolean = false): number {
      * It will return true if the expression is equal to the target parameter supplied.
      * It will NOT take into account precedence as stated in the problem definition.
      *
-     * @param target        Target value to check for at the end of the expression
-     * @param equation      An array of strings containing both the numbers and operators to evaluate
+     * @param {number}   target        Target value to check for at the end of the expression
+     * @param {string[]} equation      An array of strings containing both the numbers and operators to evaluate
      *
      * @returns {boolean}   Returns true if the equation equals the target parameter and false otherwise
      */
@@ -157,7 +115,6 @@ function parseCalibration(filePath: string, concat: boolean = false): number {
         return sum === target ? true : false;
     }
 
-    let rowAdded: Set<number> = new Set();
     let totalCalibration: number = 0;
     const planks: string = fs.readFileSync(filePath, "utf8");
 
@@ -173,23 +130,13 @@ function parseCalibration(filePath: string, concat: boolean = false): number {
                 if (target) {
                     const targetNum: number = Number.parseInt(target);
 
-                    if (concat) {
-                        const perms = generatePermutations(plankNumbers.slice(1));
-                        for (const perm of perms) {
-                            if (evaluateCalibration(targetNum, perm)) {
-                                totalCalibration += targetNum;
-                                // Stop the loop if we have an equation that works
-                                break;
-                            }
+                    const perms = generatePermutations(plankNumbers.slice(1), concat);
+                    for (const perm of perms) {
+                        if (evaluateCalibration(targetNum, perm)) {
+                            totalCalibration += targetNum;
+                            // Stop the loop if we have an equation that works
+                            break;
                         }
-                    } else {
-                        totalCalibration += addOrMultiplyDfs(
-                            targetNum,                  // Target
-                            plankNumbers,
-                            0,                          // Initial sum
-                            1,                          // The next element in the array we considering
-                            i,                          // plank index or row index
-                        );
                     }
                 }
             }
